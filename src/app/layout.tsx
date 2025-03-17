b3255346742c602becdd6e248f7623d4abbe4ed3
Luan <luan.nascimento.dev@gmail.com>
2025-03-17 14:32:09 -0300

    chore: fix image metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,17 +13,18 @@ export const metadata: Metadata = {
   description: 'Encurte seus links e compartilhe com facilidade',
   openGraph: {
     title: 'MinifyURL | Encurtador de URLs',
-    description: 'Encurte seus links e compartilhe com facilidade',
+    description:
+      'Encurte URLs longas e compartilhe links curtos de forma rápida e fácil. A melhor ferramenta de encurtamento de links para otimizar seus compartilhamentos!',
     url: 'https://minifyurl.net/',
     siteName: 'MinifyURL',
-    // images: [
-    //   {
-    //     url: 'https://seusite.com/imagem-preview.jpg',
-    //     width: 1200,
-    //     height: 630,
-    //     alt: 'Imagem de prévia do site',
-    //   },
-    // ],
+    images: [
+      {
+        url: 'https://seusite.com/minify-craw.png',
+        width: 1200,
+        height: 630,
+        alt: 'Imagem de prévia do site',
+      },
+    ],
     type: 'website',
   },
   // twitter: {
